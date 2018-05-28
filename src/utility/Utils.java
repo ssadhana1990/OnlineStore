62cd822b2a77cd3a945f57aa85337562de73aac2
SATHYANILA <SATHYANILA@SATHYANILA.Home>
2018-05-28 08:42:30 +0100

    subbb

--- src/utility/Utils.java
+++ src/utility/Utils.java
@@ -16,7 +16,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Utils {
-		public static WebDriver driver = null;
+		public static WebDriver driver;
 	public static WebDriver OpenBrowser(int iTestCaseRow) throws Exception{
 		String sBrowserName;
 		try{
